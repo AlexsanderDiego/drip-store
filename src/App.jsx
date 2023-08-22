cefb510121218788c58be9daf2c40cf424be9ad4
AlexsanderDiego <alexdiegoat@gmail.com>
2023-08-22 00:40:41 -0300

    add recado moi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import MinhaTag from "./MinhaTag"
+import MeuBotao from "./MeuBotao"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
       <div>oi</div>
       <MinhaTag />
       <h2>Atualizando...de novo</h2>
+      <MeuBotao />
     </>
   )
 }
---
+++ src/MeuBotao.jsx
@@ -0,0 +1,7 @@
+function MeuBotao() {
+    return (
+        <button onClick={() => alert('TE AMO MOI')}>Clique aqui</button>
+    )
+}
+
+export default MeuBotao;
